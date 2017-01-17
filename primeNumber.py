b88dbf48456704d9a0d9de2c10d3b6fa1585d189
georgreen <geogreenmanu@gmail.com>
2017-01-17 10:14:35 +0300

    Added two new intial files

---
+++ primeNumber.py
@@ -0,0 +1,8 @@
+'''A that generates prime number in a range(0, n)'''
+
+def primeNumber_generator(n):
+    """
+       n is a positive integer
+       outpout -> alist of primes between 0-n
+    """
+    pass
