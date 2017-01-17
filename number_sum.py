2ee9193fb9e1912d4c12f33f6c4139d2e1532397
georgreen <geogreenmanu@gmail.com>
2017-01-17 10:03:55 +0300

    Initial files

---
+++ number_sum.py
@@ -0,0 +1,8 @@
+'''
+   Input -> interger
+   output ->add_sum(interger)
+   Example -> add_sum(5) : 15
+'''
+def add_sum(n):
+    if(type(n) != int): raise TypeError
+    pass
---
+++ tests.py
@@ -0,0 +1,25 @@
+import unittest
+
+from number_sum import add_sum
+
+class test_number_sum(unittest.TestCase):
+    def test_sumPositive(self):
+        pass
+
+    def test_sumNegative(self):
+        pass
+
+    def test_sumZero(self):
+        pass
+
+    def test_nonIntegers(self):
+        pass
+
+    def test_veryLargeIntergers(self):
+        pass
+
+    def test_verysmallInterger(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
